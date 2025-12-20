72e99d58c07446ba02033afa007eff4100f0c28d
Akas Patranobis <akaspatranobis@yahoo.com>
2025-12-20 11:18:44 +0530

    up

---
+++ live-classes/terraform/remote-backend/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+  backend "s3" {
+    bucket = "aws-learning-demo-bucket "
+    key    = "suvajit/mytffiles/terraform.tfstate"
+    region = "us-east-1" 
+    
+    use_lockfile = true
+
+
+          
+  } 
+}
+
+
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+}
+
+
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0533f2ba8a1995cf9"
+  instance_type = "t2.micro"
+}
